46b90d2a034fc271e4d347eb42ab36699dec0abf
sabeom <sabumi2002@naver.com>
2021-04-07 19:25:14 +0900

    role and polymorphism for interface

---
+++ chapter2/ch03_13/domain/userinfo/dao/UserInfoDao.java
@@ -0,0 +1,9 @@
+package ch03_13.domain.userinfo.dao;
+
+import ch03_13.domain.userinfo.UserInfo;
+
+public interface UserInfoDao {
+	void insertUserInfo(UserInfo userInfo);
+	void updateUserInfo(UserInfo userInfo);
+	void deleteUserInfo(UserInfo userInfo);
+}
