ae11120854a85ab6bbbc4a82fa72f353aacd7533
sabeom <sabumi2002@naver.com>
2021-04-08 18:12:50 +0900

    exam_inheritance

---
+++ chapter2/ch03_16/Player.java
@@ -0,0 +1,17 @@
+package ch03_16;
+
+public class Player {
+	PlayerLevel level;
+	
+	public void getLevel() {
+		this.level.showLevelMessage();
+	}
+	void upgradeLevel(PlayerLevel playerLevel) {
+		this.level= playerLevel;
+	}
+	void play() {
+		
+		getLevel();
+		this.level.go(level.count);
+	}
+}
---
+++ chapter2/ch03_16/PlayerTest.java
@@ -0,0 +1,15 @@
+package ch03_16;
+
+public class PlayerTest {
+
+	public static void main(String[] args) {
+		Player player= new Player();
+		player.upgradeLevel(new BeginnerLevel());
+		player.play();
+		player.upgradeLevel(new AdvancedLevel());
+		player.play();
+		player.upgradeLevel(new SuperLevel());
+		player.play();
+	}
+
+}
