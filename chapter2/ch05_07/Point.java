05845c88d538e6f001c4280b76014ebdb1754423
sabeom <sabumi2002@naver.com>
2021-04-15 17:30:54 +0900

    generic Method

---
+++ chapter2/ch05_07/Point.java
@@ -0,0 +1,20 @@
+package ch05_07;
+
+public class Point<T, V> {
+
+	T x;
+	V y;
+	
+	Point(T x, V y){
+		this.x= x;
+		this.y= y;
+	}
+	
+	public T getX(){
+		return x;
+	}
+	
+	public V getY(){
+		return y;
+	}
+}
