5916cce26bc4a895a6617b1be7bcaf9715facdec
sabeom <sabumi2002@naver.com>
2021-06-16 16:50:03 +0900

    adapter pattern

---
+++ Spring/Main.java
@@ -0,0 +1,39 @@
+package com.company.design;
+
+import com.company.design.adapter.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        /*
+        AClazz aClazz = new AClazz();
+        BClazz bClazz = new BClazz();
+
+        SocketClient aClient = aClazz.getSocketClient();
+        SocketClient bClient = bClazz.getSocketClient();
+
+        System.out.println("두개의 객체가 동일한가?");
+        System.out.println(aClient.equals(bClient));
+        */
+
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        connect(airAdapter);
+
+        //connect(cleaner);
+    }
+
+    public static void connect(Electronic110V electronic110V){
+        electronic110V.powerOn();
+    }
+}
