4641596c6c2aea6898b22022077619edb1cd9c5e
sabeom <sabumi2002@naver.com>
2021-06-21 20:05:57 +0900

    proxy, aop

--- Spring/proxy/Main.java
+++ Spring/proxy/Main.java
@@ -1,5 +1,9 @@
 package com.company.design.proxy;
 
+import com.company.design.aop.AopBrowser;
+
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -9,11 +13,31 @@ public class Main {
         browser.show();
         browser.show();
 
-         */
+
         IBrowser browser = new BrowserProxy("www.naver.com");
         browser.show();
         browser.show();
         browser.show();
         browser.show();
+         */
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+        );
+
+        aopBrowser.show();
+        System.out.println("loading time : "+ end.get());
+
+        aopBrowser.show();
+        System.out.println("loading time : "+ end.get());
     }
 }
