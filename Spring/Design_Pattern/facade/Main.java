5f8301b1568af58b07d9fc223d3180b3aaffed48
sabeom <sabumi2002@naver.com>
2021-07-07 21:24:28 +0900

    facade

---
+++ Spring/Design_Pattern/facade/Main.java
@@ -0,0 +1,32 @@
+package com.company.design.facade;
+
+public class Main {
+    public static void main(String[] args) {
+        Ftp ftpClient= new Ftp("www.foo.co.kr", 22, "/home/etc");
+        ftpClient.connect();
+        ftpClient.moveDirectory();
+
+        Writer writer= new Writer("text.tmp");
+        writer.fileConnect();
+        writer.write();
+
+        Reader reader= new Reader("text.tmp");
+        reader.fileConnect();
+        reader.fileRead();
+
+        reader.fileDisconnect();
+        writer.fileDisconnect();
+        ftpClient.disConnect();
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //기존 코드를 간략하게 만들어줌.
+        SftpClient sftpClient= new SftpClient("www.foo.co.kr", 22, "/home/etc", "text.tmp");
+        sftpClient.connect();
+
+        sftpClient.write();
+
+        sftpClient.read();
+
+        sftpClient.disConnect();
+    }
+}
