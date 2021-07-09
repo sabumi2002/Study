346e97c52fb399d8240fa80a7c48e83ac74520a3
sabeom <sabumi2002@naver.com>
2021-07-09 18:22:44 +0900

    stategy

---
+++ Spring/Design_Pattern/strategy/NomalStrategy.java
@@ -0,0 +1,8 @@
+package com.company.design.strategy;
+
+public class NomalStrategy implements EncodingStrategy{
+    @Override
+    public String encode(String text) {
+        return text;
+    }
+}
