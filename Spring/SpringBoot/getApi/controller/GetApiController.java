670974b91996a4e3f99c0d1235a56a6c2ac0a78d
sabeom <sabumi2002@naver.com>
2021-07-18 23:41:33 +0900

    sry..

--- Spring/SpringBoot/getApi/controller/GetApiController.java
+++ Spring/SpringBoot/getApi/controller/GetApiController.java
@@ -9,7 +9,7 @@ import java.util.Map;
 @RequestMapping("/api/get")
 public class GetApiController {
 
-    @GetMapping(path= "/hello") //http://localhost:9090/api/get/hello
+    @GetMapping(path= "/hello") // http://localhost:9090/api/get/hello
     public String getHello(){
         return "get Hello";
     }
@@ -25,7 +25,7 @@ public class GetApiController {
         return pathName;
     }
 
-    //http://localhost:9090/api/get/query-param?name=예상국&email=steve@gmail.com&age=30
+    // http://localhost:9090/api/get/query-param?name=예상국&email=steve@gmail.com&age=30
     @GetMapping(path= "query-param") //무한정 뭐가 들어올지 몰라하면 Map의 형식도 있지만
     public String queryParam(@RequestParam Map<String, String> queryParam){
         StringBuilder sb= new StringBuilder();
@@ -42,7 +42,7 @@ public class GetApiController {
         return sb.toString();
     }
 
-    @GetMapping("query-param02")    //query파라미터로 들어오는 종류가 적다하면 이렇게 써도 사오간없음
+    @GetMapping("query-param02")    // query파라미터로 들어오는 종류가 적다하면 이렇게 써도 사오간없음
     public String queryParam02(
             @RequestParam String name,
             @RequestParam String email,
@@ -55,8 +55,8 @@ public class GetApiController {
         return name+ " "+ email+ " "+ age;
     }
 
-    //?name=예상국&email=steve@gmail.com&age=30
-    @GetMapping("query-param03")    //주소    이방식이 제일 많이씀. 객체를 만들어서 쿼리파라미터가 바로매칭이되는 해당의 형태
+    // ?name=예상국&email=steve@gmail.com&age=30
+    @GetMapping("query-param03")    // 주소    이방식이 제일 많이씀. 객체를 만들어서 쿼리파라미터가 바로매칭이되는 해당의 형태
     public String queryParam03(UserRequest userRequest) {  //메소드 객체로 받을거임
         System.out.println(userRequest.getName());
         System.out.println(userRequest.getEmail());
