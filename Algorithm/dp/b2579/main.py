106a72de7e56b7c310fe9435a4e90f005f829848
DESKTOP-FHPBK2E\Sabeom <sabumi2002@naver.com>
2022-06-19 00:11:59 +0900

    dp b2579_2

--- Algorithm/dp/b2579/main.py
+++ Algorithm/dp/b2579/main.py
@@ -4,16 +4,14 @@ score = []
 dp = []
 for i in range(N):
     score.append(int(input()))
-    dp.append('0')
 
-if N==0:
-    score.append('0')
-    dp.append('0')
 
-dp[0] = score[0]
-dp[1] = score[0]+score[1]
-dp[2] = max(score[1]+score[2], score[0]+score[2])
-for i in range(3, N):
-    dp[i] = max(dp[i-3]+score[i-1]+score[i], dp[i-2]+score[i])
+dp.append(score[0])
+if N>1:
+    dp.append(score[0]+score[1])
+    if N>2:
+        dp.append(max(score[1]+score[2], score[0]+score[2]))
+        for i in range(3, N):
+            dp.append(max(dp[i-3]+score[i-1]+score[i], dp[i-2]+score[i]))
 
 print(dp[N-1])
