9bf7e797c1d12dcd1be68c571d202238261396da
DESKTOP-FHPBK2E\Sabeom <sabumi2002@naver.com>
2022-07-06 22:52:19 +0900

    dp b11052_2

--- Algorithm/dp/b11052/main.py
+++ Algorithm/dp/b11052/main.py
@@ -1,2 +1,13 @@
 N = int(input())
 P = list(map(int, input().split()))
+dp =[]
+max1 = 0
+for i in range(len(P)):
+    max1 = P[i]
+    dp.append(max1)
+    if i>0:
+        for j in range(i):
+            max1 = max(max1, dp[i-1-j] + P[j])
+        dp[i] = max1
+
+print(dp[N-1])
