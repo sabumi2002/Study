6fbc7b2efa9c7a479bd2a0c2c77ecd3b120ee81a
DESKTOP-FHPBK2E\Sabeom <sabumi2002@naver.com>
2022-07-12 22:51:00 +0900

    dp b9465_2

--- Algorithm/dp/b9465/main.py
+++ Algorithm/dp/b9465/main.py
@@ -1,49 +1,32 @@
 T = int(input())
-A = [[0], [0]]
 
 for _ in range(T):
+    A = [[0], [0]]
     n = int(input())
-    dp = [[0 for _ in range(n+1)] for _ in range(2) ]
+    dp = [[0 for _ in range(n+1)] for _ in range(2)]
+
     A[0] += list(map(int, input().split()))
     A[1] += list(map(int, input().split()))
 
-    if A[0][1]+A[1][2] > A[1][1]+A[0][2]:
-        dp[0][1], dp[1][2] = A[0][1], A[1][2]
-    else:
-        dp[1][1], dp[0][2] = A[1][1], A[0][2]
+    dp[0][1] = A[0][1]
+    dp[1][1] = A[1][1]
+
+    if(n>1):
+
+        dp[1][2] = A[0][1]+A[1][2]
+        dp[0][2] = A[1][1]+A[0][2]
+
 
     for i in range(3, n+1):
-        max1 = A[1][i - 2]+A[0][i - 1]+A[1][i]
-        max2 = A[1][i - 2]+A[0][i]
-        max3 = A[0][i - 2]+A[1][i - 1]+A[0][i]
-        max4 = A[0][i - 2]+A[1][i]
-        if dp[0][i-3] > 0:
-            if max1 > max2:
-                dp[1][i-2], dp[0][i-1], dp[1][i] = A[1][i-2], A[0][i-1], A[1][i]
-                dp[0][i-2], dp[1][i-1], dp[0][i] = 0, 0, 0
-            else:
-                dp[1][i - 2], dp[0][i] = A[1][i - 2], A[0][i]
-                dp[0][i - 2], dp[1][i], dp[0][i-1], dp[1][i-1] = 0, 0, 0, 0
-        elif dp[1][i-3] > 0:
-            if max3 > max4:
-                dp[0][i - 2], dp[1][i - 1], dp[0][i] = A[0][i - 2], A[1][i - 1], A[0][i]
-                dp[1][i - 2], dp[0][i - 1], dp[1][i] = 0, 0, 0
-            else:
-                dp[0][i - 2], dp[1][i] = A[0][i - 2], A[1][i]
-                dp[1][i - 2], dp[0][i], dp[0][i-1], dp[1][i-1] = 0, 0, 0, 0
-        else:
-            maxSum = max(max1, max2, max3, max4)
-            if maxSum == max1:
-                dp[1][i - 2], dp[0][i - 1], dp[1][i] = A[1][i - 2], A[0][i - 1], A[1][i]
-                dp[0][i - 2], dp[1][i - 1], dp[0][i] = 0, 0, 0
-            elif maxSum == max1:
-                dp[1][i - 2], dp[0][i] = A[1][i - 2], A[0][i]
-                dp[0][i - 2], dp[1][i], dp[0][i-1], dp[1][i-1] = 0, 0, 0, 0
-            elif maxSum == max1:
-                dp[0][i - 2], dp[1][i - 1], dp[0][i] = A[0][i - 2], A[1][i - 1], A[0][i]
-                dp[1][i - 2], dp[0][i - 1], dp[1][i] = 0, 0, 0
-            elif maxSum == max1:
-                dp[0][i - 2], dp[1][i] = A[0][i - 2], A[1][i]
-                dp[1][i - 2], dp[0][i], dp[0][i-1], dp[1][i-1] = 0, 0, 0, 0
+        max1 = dp[0][i-3] + A[1][i - 2]+A[0][i - 1]+A[1][i]
+        max2 = dp[0][i-3] + A[1][i - 2]+A[0][i]
+        max3 = dp[1][i-3] + A[0][i - 2]+A[1][i - 1]+A[0][i]
+        max4 = dp[1][i-3] + A[0][i - 2]+A[1][i]
+        max5 = max(dp[0][i-3], dp[1][i-3]) + A[1][i-1]+A[0][i]
+        max6 = max(dp[0][i-3], dp[1][i-3]) + A[0][i-1]+A[1][i]
 
-    print(dp)
+        dp[0][i] = max(dp[0][i - 1], max(max2, max3, max5))
+        dp[1][i] = max(dp[1][i - 1], max(max1, max4, max6))
+
+
+    print(max(dp[0][n], dp[1][n]))
